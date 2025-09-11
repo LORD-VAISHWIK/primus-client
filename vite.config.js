c28821c4be156ee896cfe4fd3e2925b0479863a2
LORD-VAISHWIK <vaishwik14366@gmail.com>
2025-09-12 03:29:23 +0530

    Fix merge conflict markers in vite.config.js and source files

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   css: {
     postcss: './postcss.config.cjs'
   },
-=======
->>>>>>> 868653eff794ed4994070e51b417534b5911fcfa
   server: {
     strictPort: true,
     port: 5173
